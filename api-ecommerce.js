b68cca3f3ed1274a0e7be5c51388b969a6ce2c48
rafaeladurand <rafaeladurandcontato@gmail.com>
2025-04-13 18:11:13 -0300

    feat: connect to Socket.IO

--- api-ecommerce.js
+++ api-ecommerce.js
@@ -3,26 +3,43 @@ const bodyParser = require("body-parser");
 const productRoutes = require("./routes/product-route.js");
 const purchaseRoutes = require("./routes/purchase-route.js");
 const userRoutes = require("./routes/user-route.js");
+const app = express();
 const connectDatabase = require("./database/db.js");
+const http = require("http");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server, {});
 
 
 dotenv.config();
-
-const app = express();
-app.use(cors()); 
-
 connectDatabase();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 app.use("/api/purchases", purchaseRoutes);
 app.use("/api/users", userRoutes);
 
 
+io.on('connection', socket => {
+
+  socket.on('authenticate', name => {
+    socket.name = name;
+  });
+
+  socket.on('chat message', msg => {
+    console.log('Received message:', msg);
+    io.emit('chat message', { sentBy: msg.sentBy, content: msg.content });
+  });
+
+  socket.on('disconnect', () => {});
+});
+
+
 const port = 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Aplicação rodando na porta ${port}`);
 });
