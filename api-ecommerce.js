d92cfdcd9784d32a8fd443b1aae72efcb097260b
rafaeladurand <rafaeladurandcontato@gmail.com>
2025-04-14 08:44:57 -0300

    feat: add edit and delete message via socket

--- api-ecommerce.js
+++ api-ecommerce.js
@@ -3,18 +3,19 @@ const bodyParser = require("body-parser");
 const productRoutes = require("./routes/product-route.js");
 const purchaseRoutes = require("./routes/purchase-route.js");
 const userRoutes = require("./routes/user-route.js");
-const app = express();
 const connectDatabase = require("./database/db.js");
-const http = require("http");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const socketIo = require('socket.io');
-const server = http.createServer(app);
-const io = socketIo(server, {});
-
+const { v4: uuidv4 } = require("uuid");
+const http = require("http");
+const socketIo = require("socket.io");
 
 dotenv.config();
 connectDatabase();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -23,21 +24,44 @@ app.use("/api/products", productRoutes);
 app.use("/api/purchases", purchaseRoutes);
 app.use("/api/users", userRoutes);
 
+const messages = [];
 
-io.on('connection', socket => {
-
-  socket.on('authenticate', name => {
+io.on("connection", (socket) => {
+  socket.on("authenticate", (name) => {
     socket.name = name;
   });
 
-  socket.on('chat message', msg => {
-    console.log('Received message:', msg);
-    io.emit('chat message', { sentBy: msg.sentBy, content: msg.content });
+  socket.on("chat message", (msg) => {
+    const messageWithId = {
+      id: uuidv4(),
+      content: msg.content,
+      sentBy: msg.sentBy,
+      timestamp: new Date().toISOString(),
+    };
+    messages.push(messageWithId);
+    io.emit("chat message", messageWithId);
   });
 
-  socket.on('disconnect', () => {});
-});
+  socket.on("edit message", (updatedMsg) => {
+    const index = messages.findIndex((msg) => msg.id === updatedMsg.id);
+    if (index !== -1) {
+      messages[index].content = updatedMsg.content;
+      io.emit("edit message", messages[index]);
+    }
+  });
 
+  socket.on("delete message", (messageId) => {
+    const index = messages.findIndex((msg) => msg.id === messageId);
+    if (index !== -1) {
+      messages.splice(index, 1);
+      io.emit("delete message", messageId);
+    }
+  });
+
+  socket.on("typing", (userName) => {
+    socket.broadcast.emit("user typing", userName);
+  });
+});
 
 const port = 3000;
 server.listen(port, () => {
